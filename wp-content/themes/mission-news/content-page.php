216962c71635ebe0193495f615dd99d9a85e0ca0
sdobke <sdobke@gmail.com>
2022-09-23 17:09:40 -0300

    New "Revista" page template

--- wp-content/themes/mission-news/content-page.php
+++ wp-content/themes/mission-news/content-page.php
@@ -1,7 +1,7 @@
 <div <?php post_class(); ?>>
-	<?php do_action( 'ct_mission_news_page_before' ); ?>
-	<?php if ( is_page( 15595 ) ) {  ?>
-		<div class="header-checkout"> 
+	<?php do_action('ct_mission_news_page_before'); ?>
+	<?php if (is_page(15595)) {  ?>
+		<div class="header-checkout">
 			<h1>Assine</h1>
 			<p>Assine a Jacobin Brasil hoje, receba duas lindas edições por ano e ajude-nos a construir uma verdadeira alternativa socialista aos magnatas da mídia capitalista.</p>
 			<div style="float:none;clear:both;"></div>
@@ -11,13 +11,13 @@
 				<h1>O que eu ganho com esta assinatura?</h1>
 				<br><br>
 				<div id="suscrpcion-contenidos">
-					<?php 
-					
+					<?php
+
 					$args_sus = array(
-						'post_type'   => array('product','wc_membership_plan'),
+						'post_type'   => array('product', 'wc_membership_plan'),
 						'post_status' => 'publish',
 						'posts_per_page' => 50,
-						'fields'      => array('ids','post_content'),
+						'fields'      => array('ids', 'post_content'),
 						'tax_query'   => array(
 							'relation' => 'AND',
 							array(
@@ -29,17 +29,16 @@
 
 					);
 					$products_sus = new WP_Query($args_sus);
-					foreach($products_sus->posts as $prodsus){
-						
+					foreach ($products_sus->posts as $prodsus) {
+
 						//echo "<!--<pre>";
 						//print_r($prodsus);
 						//echo "</pre>-->";
-						
-						
-						echo '<div id="prod_'.$prodsus->ID.'" class="prod_det" style="display:none">
-						'.$prodsus->post_content.'
+
+
+						echo '<div id="prod_' . $prodsus->ID . '" class="prod_det" style="display:none">
+						' . $prodsus->post_content . '
 						</div>';
-						
 					}
 					?>
 					<!--<div class="prod_det">Clique em uma assinatura para conhecer os benefícios.</div>-->
@@ -47,29 +46,35 @@
 				</div>
 			</div>
 		</div>
-	<?php } else { ?> 
+	<?php } else { ?>
+
 	<?php } ?>
 
 	<article class="content-page">
 		<?php ct_mission_news_featured_image(); ?>
-		<div class='post-header'>
-			<h1 class='post-title'><?php the_title(); ?></h1>
-		</div>
-		<div class="post-content">
-			<?php if ( is_page( 15595 ) ) {  ?>
-				<p class="pre-tab">Tipo de assinatura</p>
-				<p class="pre-tab"> </p>
+		<?php if (is_page(21566)) {  // REVISTA 
+			include("templates/revista.php");
+		?>
+		<?php } else { ?>
+			<div class='post-header'>
+				<h1 class='post-title'><?php the_title(); ?></h1>
+			</div>
+			<div class="post-content">
+				<?php if (is_page(15595)) {  ?>
+					<p class="pre-tab">Tipo de assinatura</p>
+					<p class="pre-tab"> </p>
 
 
-			<?php } ?>
-			<?php the_content(); ?>
-			<?php wp_link_pages( array(
-				'before' => '<p class="singular-pagination">' . esc_html__( 'Pages:', 'mission-news' ),
-				'after'  => '</p>',
-			) ); ?>
-			<?php do_action( 'ct_mission_news_page_after' ); ?>
-			<?php get_sidebar( 'after-page' ); ?>
-		</div>
+				<?php } ?>
+				<?php the_content(); ?>
+				<?php wp_link_pages(array(
+					'before' => '<p class="singular-pagination">' . esc_html__('Pages:', 'mission-news'),
+					'after'  => '</p>',
+				)); ?>
+			<?php } // FIN REVISTA ?>
+			<?php do_action('ct_mission_news_page_after'); ?>
+			<?php get_sidebar('after-page'); ?>
+			</div>
 	</article>
 	<?php comments_template(); ?>
-</div> 
+</div>
